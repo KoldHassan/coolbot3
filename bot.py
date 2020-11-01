c9e1b90ac8ba13ce49fb483795f76d6ebc6efdb1
hassan0315 <70412930+hassan0315@users.noreply.github.com>
2020-11-01 11:39:01 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,10 @@ async def karenwisdom(ctx, number: int = 1):
     while i < number:
         response = random.choice(wisdom["Karen"])
         await ctx.send(response)
-        i += 1
+        if i < 4:
+          i +=1
+        elif i == 4:
+          i = number
 
 
 keep_alive.keep_alive()
